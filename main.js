a928ce4633d2aa56025cfbf024e6ee908cf82461
Yogesh Kumar <77193839+Yogesh011-R@users.noreply.github.com>
2022-05-02 14:35:47 +0530

    fixes

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/dist/css/splide.min.css';
 
+import Swiper, { Navigation, Pagination, Scrollbar } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  speed: 500,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    620: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    700: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+  },
+});
+
 var splide = new Splide('.splide', {
   perPage: 3,
   gap: '2rem',
